88d50085b8215d191fbf3f4c3a78bf6b79de41a9
David Angulo Velez <david.angulo.velez@gmail.com>
2024-09-19 22:56:20 +0200

    Working on door, very rudemantery

---
+++ Gameplay/personal_view/door.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var hitbox: CollisionShape2D = $CollisionShape2D 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		print("Click :3")
--- Menus/menu_types/parallax_backdrop.gd
+++ Menus/menu_types/parallax_backdrop.gd
@@ -3,26 +3,30 @@ extends Node2D
 @export_range(0, 50) var motion_scale: float = 5
 
 var backdrops: Array[Sprite2D] = []
+var initial_positions: Array[Vector2] = []
+var middle_of_screen: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Get the center of the viewport
+	middle_of_screen = Vector2(get_viewport().size) / 2
+	
 	# Get all children who are Sprite2D
 	for child in self.get_children():
 		if child is Sprite2D:
 			backdrops.append(child)
+			# Store their initial positions
+			initial_positions.append(child.position)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var total_backdrops = backdrops.size()
-	
-	# Get the center of the viewport
-	var middle_of_screen = Vector2(get_viewport().size) / 2	
 
 	# Calculate the offset based on the current mouse position relative to the viewport center
 	var mouse_offset = get_global_mouse_position() - middle_of_screen
-
+	
 	for i in range(total_backdrops):
-		var current_scale: float = (motion_scale * (i + 1)) / total_backdrops
-		
-		# Calculate the new position for each backdrop based on the mouse offset and scale
-		backdrops[i].position = (mouse_offset * current_scale * delta * current_scale) - backdrops[i].position
+		var current_scale: float = (motion_scale * (i + 1) * 2) / total_backdrops
+		if backdrops[i]:
+			# Calculate the new position for each backdrop based on the mouse offset and scale
+			backdrops[i].position = initial_positions[i] + (mouse_offset * current_scale * delta)
